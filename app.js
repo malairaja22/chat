b7194038fca4e2c89153d7c1acd9615034517dd1
Malai Raja D <64495589+malairaja22@users.noreply.github.com>
2022-12-04 13:04:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -135,3 +135,11 @@ app.post('/getmessage', async (req, res) => {
 app.get('/',(req,res)=>{
     res.send("<h1>Hello</h1>")
 })
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
